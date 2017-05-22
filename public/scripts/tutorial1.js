ecf23290229109a5862817c9c40a8f761bf73a96
stevegordon <tdtsgordon@gmail.com>
2017-05-22 08:47:29 -0700

    Intialize repo

---
+++ public/scripts/tutorial1.js
@@ -0,0 +1,32 @@
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commmentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
