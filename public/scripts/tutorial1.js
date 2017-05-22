dcda22a6675cdfb307a8684c89c3377c823a12d1
stevegordon <tdtsgordon@gmail.com>
2017-05-22 08:54:55 -0700

    add child components

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -3,7 +3,9 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
